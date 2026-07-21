9494dbd920e0d8d5dea15d44abfc703e451c5809
ChicoCifrado <chicocifrado@gmail.com>
2026-07-21 22:14:21 +0200

    chore: remove legacy Zig unikernel code

--- src/arch/aarch64/boot.zig
+++
@@ -1,4 +0,0 @@
-const std = @import("std");
-const builtin = @import("builtin");
-
-export var fdt_addr: u64 = 0;
--- src/gfx/test.zig
+++
@@ -1,8 +0,0 @@
-// This file collects all gfx module tests for host-based execution.
-// Build: zig build test
-
-comptime {
-    _ = @import("font.zig");
-    _ = @import("fb.zig");
-    _ = @import("compositor.zig");
-}
